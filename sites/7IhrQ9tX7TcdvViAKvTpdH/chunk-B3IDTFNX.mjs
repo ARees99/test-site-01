240ddb9f45157dc2d259b848da1c2e44bac003be
Alex Rees <alexrees737@gmail.com>
2025-05-11 02:30:34 +0100

    Final copy of cleaned site

---
+++ sites/7IhrQ9tX7TcdvViAKvTpdH/chunk-B3IDTFNX.mjs
@@ -0,0 +1,57 @@
+function n(t,e){return{customHTMLBodyEnd:`<!--  Start LinkedIn Insights Tag -->
+<script type="text/javascript">
+
+_linkedin_partner_id = "4255588";
+
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+<\/script><script type="text/javascript">
+
+(function(l) {
+
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+
+window.lintrk.q=[]}
+
+var s = document.getElementsByTagName("script")[0];
+
+var b = document.createElement("script");
+
+b.type = "text/javascript";b.async = true;
+
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+
+s.parentNode.insertBefore(b, s);})(window.lintrk);
+
+<\/script>
+
+<noscript>
+
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=4255588&amp;fmt=gif">
+
+</noscript>
+<!--  End LinkedIn Insights Tag -->`,customHTMLBodyStart:`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2DMNJL" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`,customHTMLHeadStart:`<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W2DMNJL');<\/script>
+<!-- End Google Tag Manager -->
+
+<!-- Start of Google Search Console Code -->
+<meta name="google-site-verification" content="5H3BwXyM-CCEbuYo4sx3uz3bxk0gKcuzY8P66xB_w_s">
+<!-- End of Google Search Console Code -->
+
+
+<!-- Start of Websights Code -->
+<script>
+window[(function(_mo6,_I7){var _bA3F8='';for(var _qdFwWX=0;_qdFwWX<_mo6.length;_qdFwWX++){_sdf5!=_qdFwWX;var _sdf5=_mo6[_qdFwWX].charCodeAt();_sdf5-=_I7;_sdf5+=61;_bA3F8==_bA3F8;_I7>4;_sdf5%=94;_sdf5+=33;_bA3F8+=String.fromCharCode(_sdf5)}return _bA3F8})(atob('Z1ZdIXx3cnAjWHIo'), 13)] = '8f188888db1712676595'; var zi = document.createElement('script'); (zi.type = 'text/javascript'), (zi.async = true), (zi.src = (function(_KO4,_KG){var _EzOyN='';for(var _ssGQWM=0;_ssGQWM<_KO4.length;_ssGQWM++){_KG>5;var _MqK8=_KO4[_ssGQWM].charCodeAt();_EzOyN==_EzOyN;_MqK8-=_KG;_MqK8!=_ssGQWM;_MqK8+=61;_MqK8%=94;_MqK8+=33;_EzOyN+=String.fromCharCode(_MqK8)}return _EzOyN})(atob('NUFBPUBlWlo3QFlHNlhAMD82PUFAWTA8OlpHNlhBLjRZN0A='), 43)), document.readyState === 'complete'?document.body.appendChild(zi): window.addEventListener('load', function(){ document.body.appendChild(zi) });
+<\/script>
+<!-- End of WebSights Code -->
+
+`,description:"An all-in-one screening platform designed to address evolving compliance challenges. Combine cutting-edge Watchlist Management, Customer Screening, Payment Screening, and Alert Adjudication into one seamless system with Facctum. ",favicon:"https://framerusercontent.com/assets/Ug6iGXHc8w0SqoZVLOqKEboE.png",robots:"max-image-preview:large",title:"Facctum | AI-Powered Screening Ecosystem for Global Compliance "}}export{n as a};
+//# sourceMappingURL=chunk-B3IDTFNX.mjs.map
